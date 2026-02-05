e36719aa5c4857a6053641c1dc080b20dafb9284
cB-Ashutosh-kumar <ashutosh.ku@thecodebucket.com>
2026-02-05 18:12:16 +0530

    ui:implement loader on button

--- src/components/FloatButtonComp.jsx
+++ src/components/FloatButtonComp.jsx
@@ -1,10 +1,28 @@
-import {  FloatButton } from 'antd'
-import { QuestionCircleFilled,CustomerServiceOutlined ,BellFilled} from '@ant-design/icons'
+import { Button, FloatButton } from 'antd'
+import { QuestionCircleFilled, CustomerServiceOutlined, BellFilled } from '@ant-design/icons'
+import { useSearchParams } from 'react-router-dom'
+import { useState } from 'react'
 const FloatButtonComp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      alert("form submited successfully!");
+
+    } catch (error) {
+      console.log(error.message);
+    }
+    finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <div>
       {/*   global components that can use over the components  */}
-    {/* position is fixed  */}
+      {/* position is fixed  */}
 
       {/* <FloatButton icon={<QuestionCircleFilled/>} tooltip={{
         title:"example of title",
@@ -14,14 +32,14 @@ const FloatButtonComp = () => {
 
       {/* lets create the floated */}
       <FloatButton.Group shape='circle' >
-       {/* <FloatButton type='primary'  style={{backgroundColor:"orange",insetInlineEnd:20}} icon={<QuestionCircleFilled/>} /> */}
-       {/* <FloatButton content="hello" icon={<QuestionCircleFilled/>} /> */}
-       <FloatButton  icon={<QuestionCircleFilled/>} />
+        {/* <FloatButton type='primary'  style={{backgroundColor:"orange",insetInlineEnd:20}} icon={<QuestionCircleFilled/>} /> */}
+        {/* <FloatButton content="hello" icon={<QuestionCircleFilled/>} /> */}
+        <FloatButton icon={<QuestionCircleFilled />} />
 
       </FloatButton.Group>
 
       {/*FloatedButton menu mode  */}
-       {/* <FloatButton.Group
+      {/* <FloatButton.Group
         trigger='click'
         shape='square'
         icon={<CustomerServiceOutlined/>}
@@ -30,19 +48,24 @@ const FloatButtonComp = () => {
         <FloatButton icon={<CustomerServiceOutlined/>}/>
        </FloatButton.Group> */}
 
-       {/* <section style={{height:"300vh",backgroundColor:"orange"}}>
+      {/* <section style={{height:"300vh",backgroundColor:"orange"}}>
         <FloatButton.BackTop />
        </section> */}
+      <div style={{ marginTop: "30px", paddingLeft: "30px" }}>
+        <FloatButton style={{
+          position: "relative",
+          insetBlockEnd: "auto",
+          insetBlockStart: "auto",
+          width: 32,
+          height: 32
 
-      <FloatButton  style={{
-        position:"relative",
-        insetBlockEnd:"auto",
-        insetBlockStart:"auto",
-        width:32,
-        height:32
+        }} badge={{ count: 10 }} icon={<BellFilled />} />
+      </div>
 
-      }} badge={{count:10}} icon={<BellFilled/>}/>
-       
+        <br />
+      <Button style={{width:"120px"}} type='primary' loading={isLoading} onClick={handleSubmit} >
+        {isLoading ? "submitting..." : "submit"}
+      </Button>
     </div>
   )
 }
